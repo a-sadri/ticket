ddd1319a37ebdfb36c36f2f7601c408993c6b14c
Ali Sadri <asadri721@gmail.com>
2021-12-14 13:58:45 +0330

    Setup RTK Query and getTickets endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import chalk from 'chalk';
 
 import connectDB from './config/db.js';
@@ -12,6 +13,7 @@ dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 // Routes
