9038098a0efee151017003a918352d9582b1d87e
Ali Sadri <asadri721@gmail.com>
2021-12-19 11:32:51 +0330

    Complete message component

--- frontend/src/pages/messages/Message.js
+++ frontend/src/pages/messages/Message.js
@@ -1,18 +1,35 @@
 import { useParams } from 'react-router-dom';
+import moment from 'moment';
 import { useGetMessageQuery } from '../../redux/services/messages';
 
 const Message = () => {
   const { id } = useParams();
 
-  const { data } = useGetMessageQuery(id);
+  const { data, isLoading } = useGetMessageQuery(id);
 
-  const post = JSON.stringify(data);
+  let { title, description, status, createdAt, updatedAt } = !isLoading && data;
 
-  console.log(typeof post);
-  console.log(JSON.parse(post));
-  console.log(typeof post);
-
-  return <div>{JSON.parse(post.title)}</div>;
+  return (
+    <div>
+      <div className='flex flex-col'>
+        <div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+          <div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
+            <div className='shadow overflow-hidden p-5 mt-4 bg-white border-b border-gray-200 sm:rounded-lg'>
+              <div className='mb-4'>
+                <h1 className='font-bold text-2xl'>{title}</h1>
+              </div>
+              <div>
+                <p>{description}</p>
+              </div>
+              <div className='mt-4 '>
+                <p>{moment(createdAt).format('YYYY-mm-DD hh:mm')}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Message;
