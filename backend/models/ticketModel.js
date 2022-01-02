cd4c359abe955ada369a1a8cea7954494d4f5404
Ali Sadri <asadri721@gmail.com>
2022-01-02 16:55:57 +0330

    Change ticket model

--- backend/models/ticketModel.js
+++ backend/models/ticketModel.js
@@ -5,7 +5,7 @@ const ticketSchema = new mongoose.Schema(
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
-      required: true,
+      required: false,
     },
     // category: {},
     // tags: {},
