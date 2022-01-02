5457345697f7af8dc8127482f6f07aac26012cb5
Ali Sadri <asadri721@gmail.com>
2022-01-02 11:58:40 +0330

    Change messages page

--- frontend/src/pages/messages/Messages.js
+++ frontend/src/pages/messages/Messages.js
@@ -1,4 +1,4 @@
-import moment from 'moment';
+import { dateFormat } from '../../utils/helpers';
 import { useGetMessagesQuery } from '../../redux/apiServices';
 
 import InitialAll from '../../components/InitialAll';
@@ -6,17 +6,28 @@ import InitialAll from '../../components/InitialAll';
 const Messages = () => {
   const { data, error, isLoading, isSuccess, isError } = useGetMessagesQuery();
 
+  const setData = () => {
+    let result = data.map((item) => ({
+      id: item._id,
+      title: item.title,
+      description: item.description.substring(0, 75) + '...',
+      date: dateFormat(item.createdAt),
+      status: item.status,
+    }));
+    return result;
+  };
+
   return (
     <>
       <InitialAll
         actionline={['All Messages', 'Add New Message', '/messages/add']}
-        rowHeadungs={['Title', 'Author', 'Date', 'Status']}
+        rowHeadungs={['Title', 'Description', 'Date', 'Status']}
         data={data}
         error={error}
         isLoading={isLoading}
         isError={isError}
         isSuccess={isSuccess}
-        rowData={['_id', 'title', 'status']}
+        setData={setData}
         showURL='messages'
       ></InitialAll>
     </>
